babb6cd325dee839abf13ac80c76d0eed8caf43e
Ekram Hossain <ekramhossain117@gmail.com>
2023-05-03 12:38:47 +0600

    footer added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const Footer = () => {
       return (
             <div>
-                  this is footer
+                  <footer className="mt-5">
+                        <Container fluid>
+                              <Row>
+                                    <Col className="text-center">
+                                          <p>&copy; 2023 Recipe Hunter</p>
+                                    </Col>
+                              </Row>
+                        </Container>
+                  </footer>
             </div>
       );
 };
